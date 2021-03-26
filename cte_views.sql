075309c6e3a83e0df8ed5d8eb4d51d17d536592e
Nadiiabrandt <sakunaforever@gmail.com>
2021-03-26 16:40:06 +0200

    Uploading

---
+++ cte_views.sql
@@ -0,0 +1,52 @@
+USE AdventureWorks2019
+GO
+ 
+CREATE VIEW Person.vPerson AS
+	SELECT 
+		Person.Title
+		,Person.FirstName
+		,Person.LastName
+		,EmailAddress.EmailAddress
+	FROM 
+		Person.Person 
+	JOIN 
+		Person.EmailAddress
+		ON 
+			Person.BusinessEntityID = EmailAddress.BusinessEntityID
+;
+GO
+
+WITH TestCTE1 AS
+	(
+	SELECT
+		BusinessEntityID
+		,FirstName
+		,LastName
+	FROM 
+		Person.Person
+	),
+
+TestCTE2 AS
+	(
+	SELECT
+		BusinessEntityID
+		,PhoneNumber
+	FROM 
+		Person.PersonPhone
+	)
+
+SELECT
+	Employee.BusinessEntityID
+	,Employee.NationalIDNumber
+	,Employee.JobTitle
+	,TestCTE1.Firstname
+	,TestCTE1.LastName
+	,TestCTE2.PhoneNumber
+FROM	
+	HumanResources.Employee
+LEFT JOIN TestCTE1 ON Employee.BusinessEntityID = TestCTE1.BusinessEntityID
+LEFT JOIN TestCTE2 ON Employee.BusinessEntityID = TestCTE2.BusinessEntityID
+
+
+
+
